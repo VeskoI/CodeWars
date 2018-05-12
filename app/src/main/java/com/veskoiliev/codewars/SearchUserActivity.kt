f689b7799e5aa40b11cba15d2fd7d9898b5ead99
Vesko Iliev <iliev.vesselin@gmail.com>
2018-05-12 13:42:39 +0100

    Initial commit - create a new project

---
+++ app/src/main/java/com/veskoiliev/codewars/SearchUserActivity.kt
@@ -0,0 +1,12 @@
+package com.veskoiliev.codewars
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchUserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_user)
+    }
+}
