663fac5a07b4effe82b4d4f46abf945a11087a99
Vesko Iliev <iliev.vesselin@gmail.com>
2018-05-16 12:02:00 +0100

    Add a testCommon source set to share test data between unit and instrumentation tests

---
+++ app/src/testCommon/java/com/veskoiliev/codewars/testdata/TestUser.kt
@@ -0,0 +1,12 @@
+package com.veskoiliev.codewars.testdata
+
+import com.veskoiliev.codewars.data.local.model.User
+
+object TestUser {
+
+    val user = User(username = "username", name = "name", rank = 100, leaderBoardPosition = 3, bestLanguage = "Kotlin", bestLanguagePoints = 3432)
+    val user2 = User(username = "username2", name = "name2", rank = 1, leaderBoardPosition = 2, bestLanguage = "Kotlin", bestLanguagePoints = 3432)
+    val user3 = User(username = "username3", name = "name3", rank = 10, leaderBoardPosition = 1, bestLanguage = "Kotlin", bestLanguagePoints = 3432)
+
+    val usersList = listOf(user, user2, user3)
+}
