8ef263c0d4cbc179adeb78ed0ad017b1ad60c6d9
Vesko Iliev <iliev.vesselin@gmail.com>
2018-05-18 11:24:12 +0100

    Ensure UI tests compile again by applying the correct scope to the TestAppComponent

--- app/src/androidTest/java/com/veskoiliev/di/TestAppComponent.kt
+++ app/src/androidTest/java/com/veskoiliev/di/TestAppComponent.kt
@@ -8,7 +8,9 @@ import com.veskoiliev.codewars.di.module.*
 import dagger.BindsInstance
 import dagger.Component
 import dagger.android.support.AndroidSupportInjectionModule
+import javax.inject.Singleton
 
+@Singleton
 @Component(modules = [
     AndroidSupportInjectionModule::class,
     ActivityBindingModule::class,
